967b86248179fa71d5547aa13d1fec48f73f88ed
meehir patel <meehir.patel12@gmail.com>
2020-11-22 17:04:22 +0000

    added routes and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+
+  resources :yoga_classes do
+    resources :bookings, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy] do
+    member do
+      get 'confirmation'
+    end
+  end
+
+  get 'profile', action: :show, controller: 'users'
+
 end
